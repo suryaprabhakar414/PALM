a6c32c2391fd57e6ec4996f44d89cff14c583f6e
Surya Prabhakaran <suryaprabhakar41@gmail.com>
2019-11-03 09:28:16 +0530

    Update resnet50.py

--- models/ResNet/resnet50.py
+++ models/ResNet/resnet50.py
@@ -41,7 +41,7 @@ train_set= traingen.flow_from_directory(directory='PALM/train',target_size=(224,
 val_set = valgen.flow_from_directory(directory='PALM/Val',target_size=(224,224),
                                             batch_size=2,class_mode='categorical',seed=seed)
 
-## Model Definition
+# Model Definition
 def def_model(pretrained_weights =None):
     input=Input(shape=(224,224,3))
     res=ResNet50(input_tensor=input,include_top=False,pooling='max',weights='imagenet')
@@ -55,7 +55,7 @@ def def_model(pretrained_weights =None):
     return model
 
 # Training
-model = def_model(pretrained_weights = 'PALM\Resnet50\Weights_Resnet50\Resnet50.01-0.0901-0.9615.hdf5') 
+model = def_model(pretrained_weights = 'PALM\Resnet50\Weights_Resnet50\Resnet50.01-0.0901-0.9615.hdf5')
 checkpoint =model_checkpoint = ModelCheckpoint('PALM\Resnet50\Weights_Resnet50\Resnet50.{epoch:02d}-{val_loss:.4f}-{val_acc:.4f}.hdf5', monitor='val_loss',verbose=1, save_best_only=True)
 callback_list=  [checkpoint]
 history=model.fit_generator(generator = train_set,steps_per_epoch=171,epochs=50,callbacks=callback_list,
